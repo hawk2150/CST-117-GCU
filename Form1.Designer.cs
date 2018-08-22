16d3d8cf7ab186749e957dabaeaf6f04c6c7fe45
hawk2150 <40759733+hawk2150@users.noreply.github.com>
2018-08-22 04:04:34 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.gradeButton = new System.Windows.Forms.Button();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.White;
+            this.titleLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.titleLabel.Font = new System.Drawing.Font("Buxton Sketch", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(280, 36);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(220, 35);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Welcome to Exercise 1";
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.BackColor = System.Drawing.Color.White;
+            this.questionLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.questionLabel.Font = new System.Drawing.Font("Buxton Sketch", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionLabel.Location = new System.Drawing.Point(132, 122);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(533, 28);
+            this.questionLabel.TabIndex = 3;
+            this.questionLabel.Text = "This is your first C# programming class, what grade will you receive?";
+            // 
+            // gradeButton
+            // 
+            this.gradeButton.BackColor = System.Drawing.Color.White;
+            this.gradeButton.Font = new System.Drawing.Font("Buxton Sketch", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeButton.Location = new System.Drawing.Point(307, 332);
+            this.gradeButton.Name = "gradeButton";
+            this.gradeButton.Size = new System.Drawing.Size(160, 75);
+            this.gradeButton.TabIndex = 6;
+            this.gradeButton.Text = "Click for Prediction";
+            this.gradeButton.UseVisualStyleBackColor = false;
+            this.gradeButton.Click += new System.EventHandler(this.gradeButton_Click);
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.BackColor = System.Drawing.Color.White;
+            this.gradeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gradeLabel.Font = new System.Drawing.Font("Buxton Sketch", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeLabel.Location = new System.Drawing.Point(228, 219);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(326, 61);
+            this.gradeLabel.TabIndex = 7;
+            this.gradeLabel.Text = "An \"A\" of course!!";
+            this.gradeLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.gradeButton);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Button gradeButton;
+        private System.Windows.Forms.Label gradeLabel;
+    }
+}
+
